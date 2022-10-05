68064b2fb9b31600ce95ad0046b764e395c8d8b4
Timiks1 <91879528+Timiks1@users.noreply.github.com>
2022-10-05 11:36:30 +0300

    true done

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -5,18 +5,15 @@ dateNums[0] = int.Parse(dates[1]);
 dateNums[1] = int.Parse(dates[0]);
 dateNums[2] = int.Parse(dates[2]);
 int l = 1;
-int year = 1995;
+int year = 1971;
 if (year < dateNums[2])
 {
     for (int i = year; i < dateNums[2]; i++)
     {
-        if (l < 20)
+        l += 11;
+        if (l > 30)
         {
-            l++;
-        }
-        else
-        {
-            l = 1;
+            l -= 30;
         }
     }
 }
@@ -24,22 +21,21 @@ else
 {
     for (int i = year; i > dateNums[2]; i--)
     {
-        if (l < 20)
+        l += 11;
+        if(l > 30)
         {
-            l++;
-        }
-        else
-        {
-            l = 1;
+            l -= 30;
         }
     }
 }
 
-
+Console.WriteLine(l);
 int age = dateNums[0] + dateNums[1] + l;
+Console.WriteLine(age);
 if(age > 30)
 {
     age = age - 30;
+    age += 2;
 }
 if(age > 0 && age < 7)
 {
